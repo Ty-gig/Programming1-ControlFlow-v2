7604cb167fc48da3ad27eb8cec0a7cfe1b5e929f
Ty-gig <tmoss@icstars.org>
2026-02-20 10:38:47 -0600

    Adding completed condtions.js kata

--- js-katas/katas/conditions.js
+++ js-katas/katas/conditions.js
@@ -1,12 +1,39 @@
 // Conditions kata: FizzBuzz-style function that returns an array for 1..n
 
-function fizzBuzz(n = 100) {
+function FizzBuzz(n = 100) {
+ 
+ // no negative numbers
+ if (n < 1 ){
+  throw new error('n must be positive');
+ }
   // TODO: write your solution here (replace the line below).
   // Return an array of values for 1..n following FizzBuzz rules.
   // Example: fizzBuzz(5) -> [1, 2, "Fizz", 4, "Buzz"]
   // Explain your logic choices in comments as you code.
-  throw new Error('Not implemented');
+
+  
+  const result = []
+  for(let i = 1; i <= n; i+= 1){
+    if (i % 15 === 0 ){
+      result.push("FizzBuzz")
+    }
+    else if(i % 3 ===0){
+      result.push("Fizz");
+    }else if(i % 5 ===0){
+      result.push("Buzz");
+    }else {result.push(i)
+
+    }
+    
+    
+    
+  }
+
+return(result.push)
+
+
 }
+FizzBuzz(5);
 
 globalThis.__katas = globalThis.__katas || {};
 globalThis.__katas.fizzBuzz = fizzBuzz;
